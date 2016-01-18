fd75690c8f90d7ca14938a4ec10cb38924925fbc
mtasaad01 <mtasaad@outlook.com>
2016-01-17 20:59:35 -0600

    committing the functions.

---
+++ AssemblyTwo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace AssemblyTwo
+{
+
+    public class getTypes
+    {
+
+
+        public void getTypy()
+        { 
+        //Type T = Type.GetType("AssemblyTwo.Hello");
+            //Type TT = typeof(Hello);
+            
+            Hello H = new Hello();
+            Type TTT= H.GetType();
+            PropertyInfo[] properties = TTT.GetProperties();
+            ConstructorInfo[] co = TTT.GetConstructors();
+            foreach (ConstructorInfo property in co)
+            {
+               Console.WriteLine(property.Name);
+                Console.ReadLine(); 
+            }
+        }
+
+    }
+    public  class Hello
+    {
+        protected internal  int x = 999;
+        public Hello() { }
+        public Hello(int x) { }
+    }
+
+
+    public  class Hi<T>
+    {
+         internal static int x = 999;
+
+        public static bool Calculator(T value1, T value2) { return value1.Equals(value2); }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bool Equal = Hi<string>.Calculator("ba", "ba");
+            //Console.WriteLine(Equal);
+            
+            Custmer x = new Custmer();
+            x.Firstname = "mohammed";
+            x.Lastname = "asaad";
+            Console.WriteLine(x.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    public class Custmer
+    {
+        public string Firstname { get; set; }
+        public string Lastname  { get; set; }
+
+        public override string ToString()
+        {
+            return Firstname + Lastname;
+        }
+
+    }
+}
---
+++ pr01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+public class Enums { 
+
+    public static void Main(string[] args)
+    {
+        Customer customers = new Customer();
+        customers = new Customer() { Name = "Mark", Gender = 1 };
+        customers = new Customer() { Name = "Saly", Gender = 2 };
+        List<Customer> cus = new List<Customer>();
+        cus = customers;
+        foreach (Customer customer in cus) { 
+        WriteLine(customer.Name, customer.Gender);
+        }
+
+    }
+}
+
+public class Customer
+{
+ public string Name { get; set; }
+        public int Gender { get; set; }
+    public Customer()
+    {
+        Name = 
+    }
+}
