8be19cb705288d47c376965ae4d1484623927f3c
moe <mtasaad77@gmail.com>
2021-11-26 16:46:53 -0600

    create dev branch and push to dev branch after making chaing to program.cs

--- pr01/Program.cs
+++ pr01/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
-public class Enums { 
 
+public class Enums
+{
     public static void Main(string[] args)
     {
         Customer customers = new Customer();
         customers = new Customer() { Name = "Mark", Gender = 1 };
         customers = new Customer() { Name = "Saly", Gender = 2 };
         List<Customer> cus = new List<Customer>();
-        cus = customers;
-        foreach (Customer customer in cus) { 
-        WriteLine(customer.Name, customer.Gender);
+        
+        foreach (Customer customer in cus)
+        {
+            WriteLine(customer.Name, customer.Gender);
         }
-
     }
 }
 
 public class Customer
 {
- public string Name { get; set; }
-        public int Gender { get; set; }
+    public string Name { get; set; }
+    public int Gender { get; set; }
     public Customer()
     {
-        Name = 
+        Name = "test";
     }
 }
